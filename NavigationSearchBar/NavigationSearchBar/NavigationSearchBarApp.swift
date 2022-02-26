1c57156730bc085aa2769c559c221ed4400bfcaa
LugovayaOksana <47504845+LugovayaOksana@users.noreply.github.com>
2022-02-26 12:27:19 +0300

    Add files via upload

---
+++ NavigationSearchBar/NavigationSearchBar/NavigationSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSearchBarApp.swift
+//  NavigationSearchBar
+//
+//  Created by Oksana on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
